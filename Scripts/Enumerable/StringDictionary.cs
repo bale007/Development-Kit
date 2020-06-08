3fe411b1d64c2bd63616e9aa3554b39d63d73cc6
xiongyi <tangxiongyi@yoozoo.com>
2020-06-08 10:52:42 +0800

    Add String Dictionary

---
+++ Scripts/Enumerable/StringDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Data;
+using Sirenix.OdinInspector;
+
+namespace Enumerable
+{
+    public class StringDictionary<T>
+    {
+        private Dictionary<string, T> data;
+    
+        public StringDictionary()
+        {
+            data = new Dictionary<string, T>();
+        }
+
+        public void Clear()
+        {
+            data.Clear();
+        }
+
+        public void TrySetValue(string key, T value)
+        {
+            if (!data.ContainsKey(key))
+            {
+                data.Add(key, value);
+            }
+            else
+            {
+                data[key] = value;
+            }
+        }
+
+        public T TryGetValue(string key, T def)
+        {
+            if(data.TryGetValue(key, out var result))
+            {
+                return (T) result;
+            }
+
+            return def;
+        }
+    }
+}
